54f54df09cef0839c91cb1624198bec6dd570673
darisasubhash <subbusubbu8193@gmail.com>
2026-01-30 12:32:35 +0530

    [subhash] : settingup the player

---
+++ src/model/Player.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Player {
+    private int position;
+    public Player(){
+        this.position=0;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
